5ea107ec01ab2bea9972e95520a30cf736f16c66
kenlolmen <ojjaruga@gmail.com>
2024-11-19 14:59:37 +0100

    Add new files

---
+++ MojeTwinPeaks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MojeTwinPeaks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
