69260768e024352091f497b00eaf149b26cada60
kenlolmen <ojjaruga@gmail.com>
2024-11-20 13:16:52 +0100

    Add new files

---
+++ MojeTwinPeaks/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace MojeTwinPeaks.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string PlotPath { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+    }
+}
